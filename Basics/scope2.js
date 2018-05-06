c992acc167d56488290024a2f6fc4ed2a8f1d0af
Codex411 <centerforward411@gmail.com>
2018-05-06 22:33:33 +0200

    Updated and added files

---
+++ Basics/scope2.js
@@ -0,0 +1,19 @@
+// GLobal ()
+    // Local ()
+        // Local
+    // Local
+
+//let name = 'Mikke';
+
+if (true) {
+    //let name = 'Peter';
+
+    if (true) {
+        let name = 'Jen';
+        console.log(name);
+    }
+}
+
+if (true) {
+    console.log(name);
+}
---
+++ functions/undefined-null.js
@@ -0,0 +1,26 @@
+let name;
+
+name = 'Jane'
+
+if (name === undefined) {
+    console.log('Please provide a name')
+} else {
+    console.log('Name')
+}
+
+
+console.log(name);
+
+let square = function (num) {
+    console.log(num)
+}
+
+let result = square();
+
+console.log(result)
+
+let age = 38;
+
+age = null
+
+console.log(age);
