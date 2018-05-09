10cfa9b817009b16af817f60c3f4d7787eb8f8f2
Codex411 <centerforward411@gmail.com>
2018-05-09 18:41:06 +0200

    did a challenge

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -1,14 +1,43 @@
 let myAccount = {
     name: 'Mikke',
     expenses: 0,
-    income: 0
+    income: 900
 }
 
 let addExpense = function (account, amount) {
     account.expenses = account.expenses + amount;
 }
 
-addExpense(myAccount, 2.50);
+// addIncome(account, income)
+let addIncome = function (account, income) {
+    account.income = account.income + income;
+};
 
-console.log(myAccount);
+// resetAccount(account)
+let resetAccount = function (account) {
+    account.expenses = 0;
+    account.income = 0;
+}
 
+//getAccountSummary(account)
+let getAccountSummary = function (account) {
+    let exp = account.expenses;
+    let inc = account.income;
+    let balance = inc - exp;
+    return `Account for ${account.name} has $${balance}. $${inc} in income. $${exp} in expence`
+}
+
+// addIncome
+// addExpence
+// addExpence
+// getAccountSummary
+// resetAccount
+// getAccountSummary
+
+addIncome(myAccount, 500);
+addExpense(myAccount, 50);
+addExpense(myAccount, 50);
+console.log(getAccountSummary(myAccount));
+resetAccount(myAccount);
+console.log(getAccountSummary(myAccount));
+//console.log(myAccount);
