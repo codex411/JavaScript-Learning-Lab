c0b41519c6a89b1944e95365fe32d79d59e61b01
Codex411 <centerforward411@gmail.com>
2018-05-28 21:48:54 +0200

    Updated

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,13 +1,19 @@
 const Hangman = function (word, guesses) {
-    this.word = word
+    this.word = word.toLowerCase().split('')
     this.guesses = guesses
 }
 
-
+Hangman.prototype.guessedLetters = function () {
+    let letters = ['f', 'b', 's']
+}
 
 const word1 = new Hangman('Snow', 5)
 
-const word2 = new Hangman('Car', 10)
+const word2 = new Hangman('Cat', 10)
 
 console.log(word1)
 console.log(word2)
+
+console.log(word1.guessedLetters())
+
+
