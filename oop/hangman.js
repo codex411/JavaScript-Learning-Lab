a1a0528acaaff92d65fab43035c77b6814b97f79
Codex411 <centerforward411@gmail.com>
2018-05-29 06:51:10 +0200

    Making hangman game

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,19 +1,44 @@
 const Hangman = function (word, guesses) {
     this.word = word.toLowerCase().split('')
     this.guesses = guesses
+    this.guessedLetters = ['f', 'a', 'l']
 }
 
-Hangman.prototype.guessedLetters = function () {
-    let letters = ['f', 'b', 's']
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
+
+   this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+   })
+
+   return puzzle
+   
+    
+    
+/*     printWord = []
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter)) {
+            printWord.push(letter)
+        } else if (letter === ' ') {
+            printWord.push(' ')
+        } else {
+            printWord.push('*')
+        }
+    })
+    return printWord.join('')  */
 }
 
-const word1 = new Hangman('Snow', 5)
+const word1 = new Hangman('Snow fall', 5)
 
 const word2 = new Hangman('Cat', 10)
 
 console.log(word1)
 console.log(word2)
 
-console.log(word1.guessedLetters())
+console.log(word1.getPuzzle())
 
 
