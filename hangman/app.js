3b52f39157af9e4f22edc6442f009829a3ae53a6
Codex411 <centerforward411@gmail.com>
2018-05-31 21:08:35 +0200

    Updates

--- hangman/app.js
+++ hangman/app.js
@@ -1,7 +1,16 @@
+// Primitive value: string, number, boolean, null, undefined
 
+// Object: myObject --> Object.prototype --> null
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+// Function: myFunc --> Function.prototype --> Object.prototype --> null
+// String: myString --> String.prototype --> Object.prototype --> null
+// Number: myNumber --> Number.prototype --> Object.prototype --> null
+// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
 
-const product = {
-    name: 'Influence'
-}
+const product = 'computer'
 
-console.log(product.hasOwnProperty('name'))
+console.log(product)
+
+const otherProduct = new String('Phone')
+
+console.log(otherProduct)
