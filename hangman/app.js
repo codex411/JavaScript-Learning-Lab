7d80db8b796a0131cd3d91a84857f491bb1e73f5
Codex411 <centerforward411@gmail.com>
2018-06-01 22:50:43 +0200

    Updates

--- hangman/app.js
+++ hangman/app.js
@@ -1,16 +1,30 @@
-// Primitive value: string, number, boolean, null, undefined
+// - Setup new "status" property with initial value of "playing" 
+// Creat a method for recalculating status to "playing", "finished", of "failed".
+// Call that method after a guess is processed
+// Use console.log to print the status
 
-// Object: myObject --> Object.prototype --> null
-// Array: myArray --> Array.prototype --> Object.prototype --> null
-// Function: myFunc --> Function.prototype --> Object.prototype --> null
-// String: myString --> String.prototype --> Object.prototype --> null
-// Number: myNumber --> Number.prototype --> Object.prototype --> null
-// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+// Start the game and see "playing"
+// Make two incorrect guesses and see "failed".
+// Refresh the browser and guess "c", "a" and "t" to see "finished"
 
-const product = 'computer'
+const word1 = new Hangman('Cat', 2)
+console.log(word1.getPuzzle())
 
-console.log(product)
+const toDOM = function () {
+    document.querySelector('#word-to-guess').textContent = word1.getPuzzle()
+    document.querySelector('#game-over').textContent =`Remaining guesses: ${word1.guesses}`
+}
 
-const otherProduct = new String('Phone')
 
-console.log(otherProduct)
+
+toDOM()
+
+window.addEventListener('keypress', function (e) {
+    const guess = String.fromCharCode(e.charCode)
+    word1.makeGuess(guess)
+    toDOM()
+    word1.calculateStatus()
+    console.log(word1.status)
+})
+
+// Hvis jeg ikke har flere guesses, så er status "failed"
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,13 +1,13 @@
-// Create a method for making a guess
-// 1. Should accept a character for guessing
-// 2. Should add unique guesses to list of guesses
-// 3. Should decrement the guesses left if a unique guess isn't a match
-
+// Display the puzzle to the browser instead of the console
+// Display the guesses left to the browser instead of the console
+// Separate the Hangman definition from the rest of the app (use app.js)
 const Hangman = function (word, guesses) {
     this.word = word.toLowerCase().split('')
     this.guesses = guesses
-    this.guessedLetters = []
+    this.guessedLetters = ['k']
+    this.status = 'playing'
 }
+
 
 Hangman.prototype.getPuzzle = function () {
     let puzzle = ''
@@ -35,33 +35,35 @@ Hangman.prototype.getPuzzle = function () {
     return printWord.join('')  */
 }
 
-Hangman.prototype.guess = function () {
-    console.log(arguments)
+  Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase()
+    if (!this.guessedLetters.includes(guess)) {
+        this.guessedLetters.push(guess)
+        if (!this.word.includes(guess) && this.guesses > 0) {
+                this.guesses--
+        }
+    }
+}  
+
+Hangman.prototype.calculateStatus = function () {
+    const word = this.word
+    if (this.guesses < 1) {
+         this.status = 'Failed'
+         this.guesses = 0
+         document.querySelector('#remaining-guesses').textContent =`Game Over`
+    }
+}
+
+/*  Hangman.prototype.guess = function () {
     
     for (i = 0; i < arguments.length; i++) {
-        this.guessedLetters.push(arguments[i])
-
+        if (!this.guessedLetters.includes(arguments[i].toLowerCase())) {
+            this.guessedLetters.push(arguments[i].toLowerCase())
+        }
+        
         if (!this.word.includes(arguments[i])) {
             this.guesses -= 1
         }
     }
     
-    //var args = Array.prototype.slice.call(arguments);
-    //this.guessedLetters.push(args)
-}
-
-const word1 = new Hangman('Cat', 2)
-// Guess c, t, z
-word1.guess('c', 't', 'z')
-console.log(word1.getPuzzle()) // c*t
-// Print remaining guesses
-console.log(`Remaining guesses: ${word1.guesses}`)
-console.log(word1.guessedLetters)
-
-const word2 = new Hangman('New Jersey', 4)
-// Guess w
-word2.guess('w')
-console.log(word2.getPuzzle()) // **w ******
-console.log(`Remaining guesses: ${word2.guesses}`)
-
-
+}   */
