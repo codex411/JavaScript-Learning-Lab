ba8d0a93f0c1662b6bec7c1467fae9a56b950d96
Codex411 <centerforward411@gmail.com>
2018-06-06 21:47:48 +0200

    Updates

--- hangman/app.js
+++ hangman/app.js
@@ -12,3 +12,16 @@ window.addEventListener('keypress', function (e) {
     guessesEl.textContent = word1.statusMsg
 })
 
+//MAking a HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechanged', (e) => {
+    if (e.target.readyState === 4) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
+
