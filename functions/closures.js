4616de7a2446ec53c42ac8207f7060f5139e1955
Codex411 <centerforward411@gmail.com>
2018-06-11 21:50:34 +0200

    Updates

--- functions/closures.js
+++ functions/closures.js
@@ -35,4 +35,19 @@ console.log(add10(20))
 const add100 = createAdder(100)
 console.log(add100(-90))
 
-//Create createTipper wich takes 
+//Create createTipper which takes in the base tip (.15 for 15% tip)
+// Set it up to return a function that takes in the bill amount
+// Call createTipper to generate a few functions for different percentages
+// Use the generated functions to calculate tips and print them
+
+const createTipper = (a) => {
+    return (b) => {
+        return a * b
+    }
+}
+
+const tip15 = createTipper(.15)
+const tip20 = createTipper(.50)
+
+console.log(tip15(100))
+console.log(tip20(200))
